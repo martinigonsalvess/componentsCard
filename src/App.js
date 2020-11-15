cd9c45c143574ce834e848590fdd1d70568fb5f3
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2020-11-15 23:15:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Card from './components/Card';
+ 
 
 function App() {
+  const[name, setName] = useState('your name');
+  const[job, setJob] = useState('your job');
+  const[about, setAbout] = useState('The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. ');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Card name={'martin'} job={'fullstack developer'} about={about}/>
     </div>
   );
 }
